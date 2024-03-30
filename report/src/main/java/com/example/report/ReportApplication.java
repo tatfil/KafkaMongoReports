7dd3b82b46fdf12107bbb39d62c49cb3bd15669b
Tatiana Filippova <serenadeno6@gmail.com>
2024-03-30 20:38:49 +0100

    transforming into docker images

--- report/src/main/java/com/example/report/ReportApplication.java
+++ report/src/main/java/com/example/report/ReportApplication.java
@@ -1,13 +1,16 @@
 package com.example.report;
 
+import com.example.report.repository.ReportRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@EnableMongoRepositories(basePackageClasses = ReportRepository.class)
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ReportApplication {
 	private static final Logger logger = LoggerFactory.getLogger(ReportApplication.class);
 	public static void main(String[] args) {
--- report/src/main/java/com/example/report/repository/ReportRepository.java
+++ report/src/main/java/com/example/report/repository/ReportRepository.java
@@ -2,7 +2,8 @@ package com.example.report.repository;
 
 import com.example.report.model.Report;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface ReportRepository extends MongoRepository<Report, Long> {
 }
